569a60fb33423b5126fab956de88ee1f5ce692d4
Chanse123 <s556682@nwmissouri.edu>
2026-04-15 23:55:38 -0500

    still fixing

--- routes/costumes.js
+++ routes/costumes.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var costume_controllers = require('../controllers/costume');
 
-const costume_controller = require('../controllers/costume');
-
 router.get('/', costume_controller.costume_view_all_Page);
 router.get('/detail', costume_controller.costume_view_one_Page);
 router.get('/create', costume_controller.costume_create_Page);
@@ -11,3 +9,4 @@ router.get('/update', costume_controller.costume_update_Page);
 router.get('/delete', costume_controllers.costume_delete_Page);
 
 module.exports = router;
+
