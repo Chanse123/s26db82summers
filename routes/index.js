cdeae7ba33f6980620b9145fa4edf811fee36c9a
Chanse123 <s556682@nwmissouri.edu>
2026-04-15 23:44:00 -0500

    fixing render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/costumes');
+  res.render('costumes', { title: 'Costumes', results: items });
 });
 
 module.exports = router;
