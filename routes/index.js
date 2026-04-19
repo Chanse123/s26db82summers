ea33dcbaeef807226e993459c941a57d75c105db
Chanse123 <s556682@nwmissouri.edu>
2026-04-19 00:45:11 -0500

    added authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
-/* GET home page */
-router.get('/', function(req, res, next) {
-  res.redirect('/costumes');
+router.get('/', function (req, res) {
+  res.render('index', { 
+    title: 'Costume App', 
+    user: req.user 
+  });
+});
+
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Register' });
+});
+
+router.post('/register', function(req, res) {
+  Account.findOne({ username: req.body.username })
+    .then(function(user) {
+      if (user) {
+        return res.render('register', {
+          title: 'Registration',
+          message: 'Existing User',
+          account: req.body.username
+        });
+      }
+
+      let newAccount = new Account({ username: req.body.username });
+
+      Account.register(newAccount, req.body.password, function(err, user) {
+        if (err) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'Error',
+            account: req.body.username
+          });
+        }
+        res.redirect('/');
+      });
+    })
+    .catch(function(err) {
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Error',
+        account: req.body.username
+      });
+    });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login', user: req.user });
+});
+
+router.post('/login', 
+  passport.authenticate('local'), 
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+router.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+router.get('/ping', function(req, res) {
+  res.status(200).send("pong");
 });
 
 module.exports = router;
